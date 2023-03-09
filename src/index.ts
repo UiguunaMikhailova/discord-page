6cb5d698370b2a28913344554fc936e6239d7547
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2023-03-10 03:08:51 +0700

    feat: add default modal window

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,21 @@ import "./styles/style.css";
 import data from "./data.json";
 
 const menuConfigsBtn = document.getElementById("menu-button");
-const menuConfigsIcon = document.getElementById("menu-icon") as HTMLElement;
+const menuConfigsIcon = document.getElementById("menu-icon");
 const menuConfigs = document.getElementById("menu");
 const menuConfigsItems = document.querySelectorAll("#menu__item");
-const serverList = document.getElementById("server__list") as HTMLElement;
-const canals = document.getElementById("canals") as HTMLElement;
-const serverTitle = document.getElementById("server__title") as HTMLElement;
-const canalTitle = document.getElementById("canal__title") as HTMLElement;
-const online = document.getElementById("online") as HTMLElement;
-const offline = document.getElementById("offline") as HTMLElement;
-const input = document.getElementById("input") as HTMLInputElement;
-const messages = document.getElementById("messages") as HTMLInputElement;
+const serverList = document.getElementById("server__list");
+const canals = document.getElementById("canals");
+const serverTitle = document.getElementById("server__title");
+const canalTitle = document.getElementById("canal__title");
+const online = document.getElementById("online");
+const offline = document.getElementById("offline");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+const modal = document.getElementById("modal");
+const modalBtn = document.getElementById("modal__close");
+const modalOpeners = document.querySelectorAll("#modal__btn");
+const modalBox = document.getElementById("modal__box");
 
 type Message = {
   userId: number;
@@ -38,7 +42,7 @@ type Server = {
   icon: string;
   users: User[];
   groups: Group[];
-}
+};
 type User = {
   id: number;
   username: string;
@@ -56,8 +60,10 @@ showPage();
 function showPage(): void {
   currCanal.canal = 0;
   currCanal.group = 0;
-  while (serverList.firstChild) {
-    serverList.removeChild(serverList.firstChild);
+  if (serverList) {
+    while (serverList.firstChild) {
+      serverList.removeChild(serverList.firstChild);
+    }
   }
   data.forEach((item) => {
     renderServer(item.icon, item.title, item.id);
@@ -97,49 +103,63 @@ function renderUsers(users: User[], isOnline: boolean): void {
     userElement.className = `flex p-1 rounded-md hover:bg-color-gray-300 hover:opacity-100 group cursor-pointer items-center gap-4 relative ${
       isOnline ? "" : "opacity-70"
     }`;
-    userElement.innerHTML = `<img class="rounded-full w-8 h-8" src="${user.avatar}">
+    userElement.innerHTML = `<img class="rounded-full w-8 h-8" src="${
+      user.avatar
+    }">
     <p class="w-full text-color-gray-200 group-hover:text-color-white ${
       isOnline ? "online" : ""
     }">${user.username}</p>`;
     usersList.appendChild(userElement);
   });
-  if (isOnline) {
-    while (online.firstChild) {
-      online.removeChild(online.firstChild);
+  if (online && offline) {
+    if (isOnline) {
+      while (online.firstChild) {
+        online.removeChild(online.firstChild);
+      }
+      online.append(statusElement, usersList);
+    } else {
+      while (offline.firstChild) {
+        offline.removeChild(offline.firstChild);
+      }
+      offline.append(statusElement, usersList);
     }
-    online.append(statusElement, usersList);
-  } else {
-    while (offline.firstChild) {
-      offline.removeChild(offline.firstChild);
-    }
-    offline.append(statusElement, usersList);
   }
 }
 
 function renderGroups(groups: Group[], title: string): void {
-  serverTitle.innerHTML = title;
-  while (canals.firstChild) {
-    canals.removeChild(canals.firstChild);
+  (serverTitle as HTMLElement).innerHTML = title;
+  if (canals) {
+    while (canals.firstChild) {
+      canals.removeChild(canals.firstChild);
+    }
   }
   groups.forEach((group, index) => {
     const groupIndex = index;
     const groupElement = document.createElement("li");
-    groupElement.addEventListener('click', (e) => {
-      const elem = (e.target as HTMLElement).children[0]
-      if (elem.classList.contains('-rotate-90')) {
-        elem.classList.remove('-rotate-90')
-        renderCanals(group.canals, groupIndex, canalList, group.type, true);
-      } else {
-        elem.classList.add('-rotate-90')
-        renderCanals(group.canals, groupIndex, canalList, group.type, false);
-      }
-    })
     groupElement.innerHTML = `<p class="text-xs mb-2 hover:text-color-gray-100 cursor-pointer flex items-center gap-1">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="0" height="0" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
       </svg>
     ${group.title.toUpperCase()}
 </p>`;
+    groupElement.addEventListener("click", (e) => {
+      let elem;
+      const t = e.target as HTMLElement;
+      if (t.nodeName === "svg") {
+        elem = t;
+      } else if (t.nodeName === "path") {
+        elem = t.parentElement;
+      } else {
+        elem = t.children[0];
+      }
+      if (elem?.classList.contains("-rotate-90")) {
+        elem?.classList.remove("-rotate-90");
+        renderCanals(group.canals, groupIndex, canalList, group.type, true);
+      } else {
+        elem?.classList.add("-rotate-90");
+        renderCanals(group.canals, groupIndex, canalList, group.type, false);
+      }
+    });
     const canalList = document.createElement("ul");
     canalList.className = "flex flex-col gap-1";
     groupElement.appendChild(canalList);
@@ -155,13 +175,15 @@ function renderCanals(
   groupType: string,
   isVisible: boolean
 ): void {
-  canalList.innerHTML = ''
+  canalList.innerHTML = "";
   if (isVisible) {
     canals.forEach((canal, index) => {
-    createCanals(canal, canalList, index, groupIndex, groupType);
-  });
+      createCanals(canal, canalList, index, groupIndex, groupType);
+    });
   } else {
-    const canal = canals.find((canal, index) => currCanal.canal === index && currCanal.group === groupIndex)
+    const canal = canals.find(
+      (_, index) => currCanal.canal === index && currCanal.group === groupIndex
+    );
     if (canal) {
       createCanals(canal, canalList, currCanal.canal, groupIndex, groupType);
     }
@@ -190,10 +212,12 @@ function createCanals(
       : ""
   }`;
   if (currCanal.canal === index && currCanal.group === groupIndex) {
-    canalTitle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="1" height="1" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+    (
+      canalTitle as HTMLElement
+    ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="1" height="1" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
     <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
     </svg><p>${canal.title}</p>`;
-    input.placeholder = `Написать #${canal.title}`;
+    (input as HTMLInputElement).placeholder = `Написать #${canal.title}`;
   }
   canalElement.className = classes;
   let svg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="1" height="1" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
@@ -210,70 +234,108 @@ function createCanals(
 }
 
 function renderCanal(): void {
-  while (messages.firstChild) {
-    messages.removeChild(messages.firstChild);
+  if (messages) {
+    while (messages.firstChild) {
+      messages.removeChild(messages.firstChild);
+    }
   }
   const server = data.find((server) => server.id === currServer);
   const group = server?.groups.find((group) => group.id === currCanal.group);
   const canal = group?.canals.find((canal) => canal.id === currCanal.canal);
-  const messageList = document.createElement('ul')
-  messageList.className = 'flex flex-col-reverse gap-3 my-3 overflow-y-auto';
+  const messageList = document.createElement("ul");
+  messageList.className = "flex flex-col-reverse gap-3 my-3 overflow-y-auto";
   canal?.messages.reverse().forEach((message) => {
-    const user = server?.users.find((x) => x.id === message.userId)
-    const messageElement = document.createElement('li')
-    messageElement.className = 'flex hover:bg-color-gray-400 px-4 py-2'
+    const user = server?.users.find((x) => x.id === message.userId);
+    const messageElement = document.createElement("li");
+    messageElement.className = "flex hover:bg-color-gray-400 px-4 py-2";
     messageElement.innerHTML = `<img class="w-10 h-10 mr-4" src="${user?.avatar}"/>
     <div>
       <p class="mr-2">${user?.username}<span class="text-xs ml-3 font-light text-color-gray-200">${message.date}</span></p>
       <p class="text-sm font-normal">${message.text}</p>
-    </div>`
-    messageList.appendChild(messageElement)
-  })
+    </div>`;
+    messageList.appendChild(messageElement);
+  });
   if (!canal?.messages.length) {
-    const messageElement = document.createElement('div')
-    messageElement.className = 'flex flex-col justify-center items-center gap-5 mb-5'
+    const messageElement = document.createElement("div");
+    messageElement.className =
+      "flex flex-col justify-center items-center gap-5 mb-5";
     messageElement.innerHTML = `
       <p class="text-2xl text-center">Добро пожаловать на сервер</p>
-      <p class="">Это начало истории этого канала.</p>`
-    messageList.appendChild(messageElement)
+      <p class="">Это начало истории этого канала.</p>`;
+    messageList.appendChild(messageElement);
   }
-  messages.appendChild(messageList);
-  messages.scrollTop = messages.scrollHeight;
+  if (messages) {
+    messages.appendChild(messageList);
+    messages.scrollTop = messages.scrollHeight;
+  }
 }
 
-menuConfigsBtn?.addEventListener("click", () => {
+function toggleMenu(): void {
   if (menuConfigs?.classList.contains("hidden")) {
     menuConfigs?.classList.remove("hidden");
     menuConfigs?.classList.add("flex");
-    menuConfigsIcon.innerHTML = `<svg class="-mr-1 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+    (
+      menuConfigsIcon as HTMLElement
+    ).innerHTML = `<svg class="-mr-1 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
   </svg>
   `;
   } else {
     menuConfigs?.classList.add("hidden");
     menuConfigs?.classList.remove("flex");
-    menuConfigsIcon.innerHTML = `<svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+    (
+      menuConfigsIcon as HTMLElement
+    ).innerHTML = `<svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
     </svg>`;
   }
+}
+
+function openModal(): void {
+  modal?.classList.add("active");
+}
+
+menuConfigsBtn?.addEventListener("click", (e) => {
+  e.stopPropagation();
+  toggleMenu();
 });
 menuConfigsItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    menuConfigs?.classList.add("hidden");
-    menuConfigs?.classList.remove("flex");
-    menuConfigsIcon.innerHTML = `<svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-    </svg>`;
-  })
-})
-
-window.addEventListener('click', (e) => {
-  const target = e.target as HTMLElement
-  if (menuConfigs?.classList.contains('flex') && target !== menuConfigsBtn) {
-    menuConfigs?.classList.add("hidden");
-    menuConfigs?.classList.remove("flex");
-    menuConfigsIcon.innerHTML = `<svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-    </svg>`;
+  item.addEventListener("click", (e) => {
+    e.stopPropagation();
+    toggleMenu();
+    openModal();
+  });
+});
+modalOpeners.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.stopPropagation();
+    openModal();
+  });
+});
+window.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const target = e.target as HTMLElement;
+  if (
+    menuConfigs?.classList.contains("flex") &&
+    target !== menuConfigsBtn &&
+    target !== menuConfigsIcon
+  ) {
+    toggleMenu();
+  }
+  if (modal?.classList.contains("active")) {
+    modal.classList.remove("active");
+  }
+});
+modalBox?.addEventListener("click", (e) => e.stopPropagation());
+modalBtn?.addEventListener("click", (e) => {
+  e.stopPropagation();
+  modal?.classList.remove("active");
+});
+input?.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    (input as HTMLInputElement).value = ''
   }
 })
+window.addEventListener("load", () =>
+  document.body.classList.remove("preload")
+);
